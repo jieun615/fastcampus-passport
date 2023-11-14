cbbe28a8fb9def7e498826972d76c357ade842cf
jieun615 <je060105@gmail.com>
2023-11-14 15:16:38 +0900

    Passport를 이용한 로그인 회원가입 기능 구현하기

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const { default: mongoose } = require('mongoose');
 const app = express();
 const path = require('path');
+const User = require('./models/users.model');
+const passport = require('passport');
+
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
@@ -23,8 +29,37 @@ app.get('/login', (req, res) => {
     res.render('login');
 })
 
+app.post('/login', (req, res, next) => {
+    passport.authenticate("local", (err, user, info) => {
+        if(err) {
+            return next(err);
+        }
+        if(!user) {
+            return res.json({ msg: info})
+        }
+        req.logIn(user, function (err) {
+            if(err) { return next(err); }
+            res.redirect('/');
+        })
+    })
+})
+
 app.get('/signup', (req, res) => {
     res.render('signup');
+})
+
+app.post('/signup', async (req, res) => {
+    //유저 객체 생성
+    const user = new User(req.body)
+    try{
+        //유저 컬랙션에 유저를 저장
+        await user.save();
+        return res.status(200).json({
+            success: true
+        })
+    } catch (error) {
+        console.error(error);
+    }
 })
 
 const port = 3000;
